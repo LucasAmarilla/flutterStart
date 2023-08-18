2fa9a15e76ce1cf507bc970205d279a65ef8287d
Lucas Amarilla <amarilla482@gmail.com>
2023-08-18 15:20:44 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# testapp
+
+Projeto flutter do Bootcamp Santander
+
+## O que ele faz
+
+Mostra quantas vezes o botão + foi apertado
---
+++ android/app/src/main/kotlin/tech/lucasamarilla/testapp/testapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package tech.lucasamarilla.testapp.testapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
